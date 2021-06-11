81230fd73cc6845cc24d14a80bb895a6ea9b17c7
xiborra <61407026+xiborra@users.noreply.github.com>
2021-06-11 17:06:43 +0200

    Init project

---
+++ src/main/java/com/xiborra/similarproducts/SimilarProductsApplication.java
@@ -0,0 +1,13 @@
+package com.xiborra.similarproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimilarProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimilarProductsApplication.class, args);
+	}
+
+}
